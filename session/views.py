90cf2910bcf3ca219dcd498908b5382e97571309
Eddine Omar <eddine.farid@hotmail.fr>
2021-06-04 21:48:59 +0300

    update view

--- session/views.py
+++ session/views.py
@@ -1,37 +1,46 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 from session.models import Session
 from session.serializers import SessionSerializer
+from django.http import Http404
 
-class SessionApiView(APIView):
-    # add permission to check if user is authenticated
-    permission_classes = [permissions.IsAuthenticated]
 
-    # 1. List all
-    def get(self, request, *args, **kwargs):
-        '''
-        List all the todo items for given requested user
-        '''
-        # sessions = Session.objects.filter(user = request.user.id)
-        sessions = Session.objects.all()
-        serializer = SessionSerializer(sessions, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class Sessions(APIView):
+    def get(self, request, pk=None):
+        if pk is None:
+            instances = Session.objects.all()
+            serializer = SessionSerializer(
+                instances, many=True, context={"request": request}
+            )
+        else:
+            instances = get_object_or_404(Session.objects.filter(pk=pk))
+            serializer = SessionSerializer(instances, context={"request": request})
+        return Response(serializer.data)
 
-    # 2. Create
-    def post(self, request, *args, **kwargs):
-        '''
-        Create the Todo with given todo data
-        '''
-        data = {
-            'date': request.data.get('date'), 
-            'instructor_id': request.data.get('instructor_id'), 
-            'student_id': request.data.get('student_id')
-        }
-        serializer = SessionSerializer(data=data)
+    def post(self, request):
+        serializer = SessionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        instance = get_object_or_404(Session.objects.all(), pk=pk)
+        serializer = SessionSerializer(instance=instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def patch(self, request):
+    #     serializer = SessionSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

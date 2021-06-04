1ca8cde5a3eb672aff00ce82a9b778202517be44
Eddine Omar <eddine.farid@hotmail.fr>
2021-06-04 18:39:39 +0300

    update model session

--- session/models.py
+++ session/models.py
@@ -7,10 +7,12 @@ from django.utils import timezone
 
 # Create your models here.
 class Session(models.Model):
-    start_date = models.DateTimeField(default=timezone.now)
-    end_date = models.DateTimeField(default=timezone.now)
+    start_date = models.DateTimeField(null=False)
+    end_date = models.DateTimeField(null=False)
     location = models.CharField(max_length=75, blank=True, null=True)
     instructor_id = models.ForeignKey(Instructor, on_delete=CASCADE)
     student_id = models.ForeignKey(Student, on_delete=CASCADE)
     active = models.BooleanField(default=True)
-    debrief = models.CharField(max_length=75, blank=True, null=True)
+    debrief = models.CharField(max_length=1024, blank=True, null=True)
+    created = models.DateField(default=timezone.now)
+    private_coment = models.CharField(max_length=1024, blank=True, null=True)

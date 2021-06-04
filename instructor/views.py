18f7caf082743f3911c56afcff7dfa1680a1fb25
Eddine Omar <eddine.farid@hotmail.fr>
2021-06-04 19:11:58 +0300

    update instructors view

--- instructor/views.py
+++ instructor/views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework.generics import get_object_or_404
 from instructor.models import Instructor
 from instructor.serializers import InstructorSerializer
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-class InstructorViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = Instructor.objects.all()
-    serializer_class = InstructorSerializer
-    permission_classes = [permissions.IsAuthenticated]
+class InstructorViewSet(APIView):
+    def get(self, request, pk=None, format=None):
+        if pk is None:
+            snippets = Instructor.objects.all()
+            serializer = InstructorSerializer(snippets, many=True, context={'request': request})
+        else:
+            print(pk)
+            snippets = get_object_or_404(Instructor.objects.all(), pk=pk)
+            serializer = InstructorSerializer(snippets, context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = InstructorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None, format=None):
+        instance = get_object_or_404(Instructor.objects.all(), pk=pk)
+        serializer = InstructorSerializer(instance=instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

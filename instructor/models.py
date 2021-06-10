3e6ce358ed1b4e269c343b6f4aabcd892c428d53
Eddine Omar <eddine.farid@hotmail.fr>
2021-06-10 19:10:45 +0300

    add models field

--- instructor/models.py
+++ instructor/models.py
@@ -5,3 +5,4 @@ class Instructor(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=14)
+    email = models.EmailField()

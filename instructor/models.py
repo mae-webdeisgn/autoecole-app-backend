400fe7930f753c652aa2c4ebd88518d7b9d5afe1
Eddine Omar <eddine.farid@hotmail.fr>
2021-06-11 16:09:35 +0300

    update models and migrations

--- instructor/models.py
+++ instructor/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Instructor(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=14)
-    email = models.EmailField()
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    phonenumber = models.CharField(max_length=14)
+    email = models.EmailField(default="contact@izidi.com")

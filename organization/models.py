f912ea6fe4630da8a0b5b024797de39080b1bd27
Eddine Omar <eddine.farid@hotmail.fr>
2021-06-29 08:20:27 +0300

    update model and migration organization

--- organization/models.py
+++ organization/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Organization(models.Model):
-    name = models.CharField(max_length=50, default="")
-    phone = models.CharField(max_length=14, default="")
-    address = models.CharField(max_length=150, default="")
+    name = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=14, null=True)
+    address = models.CharField(max_length=150, null=True)
     email = models.EmailField()

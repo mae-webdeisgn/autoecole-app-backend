aaeabe01de0a4cccaf911574fcadcf374d9364b8
Eddine Omar <eddine.farid@hotmail.fr>
2021-06-29 08:58:27 +0300

    update session model and make migration

--- session/models.py
+++ session/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from django.utils import timezone
 from users.models import User
-
+from organization.models import Organization
 
 # Create your models here.
 class Session(models.Model):
@@ -15,3 +15,4 @@ class Session(models.Model):
     debrief = models.CharField(max_length=1024, blank=True, null=True)
     created = models.DateField(default=timezone.now)
     private_coment = models.CharField(max_length=1024, blank=True, null=True)
+    organization = models.ForeignKey(Organization, related_name="student_id", on_delete=models.CASCADE, null=True)

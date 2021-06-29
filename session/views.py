e9da9cbc9d9a4e8635cca40b58c5d0e62154a362
Eddine Omar <eddine.farid@hotmail.fr>
2021-06-29 21:03:34 +0300

    remove comment

--- session/views.py
+++ session/views.py
@@ -36,11 +36,3 @@ class Sessions(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # def patch(self, request):
-    #     serializer = SessionSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
